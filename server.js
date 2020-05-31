50423da9991d837a5a442067d0c4def387b65a00
Ritik Verma <ritikv2@gmail.com>
2020-05-31 18:05:46 +0530

    use sqlite db for tasks
    Signed-off-by: Ritik Verma <ritikv2@gmail.com>

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const { db, Tasks } = require('./db')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extenden: true}))
+
+const PORT = process.env.PORT || 3434
+
+app.get('/', (req, res) =>{
+    res.send('Hello World')
+})
+
+app.get('/tasks', async (req, res) => {
+    res.send(await Tasks.findAll())
+})
+
+app.post('/tasks', async (req, res) => {
+    res.send(await Tasks.create(req.body))
+})
+
+db.sync()
+    .then(() => {
+
+        app.listen(PORT, (req, res) => {
+            console.log(`Server started on http://localhost:${PORT}`)
+        }) 
+    })
